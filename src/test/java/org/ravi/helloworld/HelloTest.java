dfa9dffe4db9e2c86ffecb1a6fc575598e7f3d35
Manoharan <srividya.manoharan@accenture.com>
2018-07-11 11:41:07 +0530

    new commit

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -29,8 +29,9 @@ public class HelloTest {
 	@Test
 	public void f() {
 
-		String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
-		System.out.println("The date is: "+text1);
+		
+		//String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
+		//System.out.println("The date is: "+text1);
 		driver.findElement(By.name("userName")).sendKeys("vishnu");
 		String text = driver.findElement(By.name("userName")).getAttribute("value");
 		System.out.println("The Username entered is: "+text);
